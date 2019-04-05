e78de47509742224c67886dcaeabead0dd6fe97c
unknown <lucas.singh10@gmail.com>
2019-04-05 02:02:38 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  name:string = '';
+
+  constructor(private httpClient:HttpClient) { }
+
+  onNameKeyUp(event:any) {
+        this.name = event.target.value;
+      }
+      getProfile() {
+        //console.log(this.name);
+        this.httpClient.get(`https://jsonplaceholder.typicode.com/users/?name=${this.name}`)
+        .subscribe(
+          (data:any[]) => {
+            console.log(data);
+          }
+        )
+      }
 }
